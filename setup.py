e7888a7ba2f4289c0a42dff1442c199d3521fbe7
todikun <todirahmat123@gmail.com>
2022-06-02 15:06:44 +0700

    stable version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="earthquake-idbmkg",
-    version="0.0.1",
+    version="0.0.2",
     author="Todi Rahmat",
     author_email="todirahmat123@gmail.com",
     description="This package will get the latest eartquake data from BMKG | Indonesia Meteorological, Climatological, "
